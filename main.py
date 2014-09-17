218201dd312eea60d8eae9a229101e3d0dfcc941
Hendrik Heuer <hendrikheuer@jaz5.kyla.fi>
2014-09-17 18:12:06 +0300

    more explorations with ngrams

--- main.py
+++ main.py
@@ -4,44 +4,51 @@ import string
 import nltk
 from nltk.stem import WordNetLemmatizer
 import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
 
 with open('pulman.txt', 'r') as f:
-    read_data = f.read()
+    article = f.read().split()
 
 STOP_WORDS = nltk.corpus.stopwords.words('english') + list( string.punctuation )
-tokens = [i.decode('utf-8') for i in nltk.word_tokenize( read_data.lower() ) if i not in STOP_WORDS]
 
-wnl = WordNetLemmatizer()
-tokens_lemmatized = [ wnl.lemmatize( i ) for i in tokens]
+#tokens = [i.decode('utf-8') for i in nltk.word_tokenize( article.lower() ) if i not in STOP_WORDS]
 
-# model = word2vec.load("data/vectors.bin")
+G = nx.Graph()
 
-for i in xrange(len(tokens)):
-    if tokens[i] != tokens_lemmatized[i]:
-        print tokens[i], tokens_lemmatized[i]
+# remove stop words and punctation
+for index in xrange(len(article)):
+    article[ index ] = article[ index ].translate(string.maketrans("",""), string.punctuation).lower()
 
-"""
+article = article[0:300]
 
-tagged = nltk.pos_tag(tokens)
+# add nodes
 
-nouns = []
-verbs = []
+previous_ngram = ""
 
-for entity,tag in tagged:
-    if "NN" in tag:
-        #print entity, tag
-        nouns.append( entity )
-    if "VB" in tag:
-        #print entity, tag
-        verbs.append( entity )
+for ngram_index in xrange(len(article)-3):
+    if ngram_index == 0:
+        ngram = "_".join( article[ngram_index: ngram_index + 3] )
+        next_ngram = "_".join( article[ngram_index + 1 : ngram_index + 1 + 3] )
 
-print "---"
+        G.add_node( ngram )
+        G.add_node( next_ngram )
+    else:
+        previous_ngram = ngram
+        ngram = next_ngram
+        next_ngram = "_".join( article[ngram_index + 1 : ngram_index + 1 + 3] )
 
-for elem in nouns:
-    print elem
+        G.add_node( next_ngram )
 
-print "---"
+        G.add_edge( previous_ngram, ngram )
+        G.add_edge( ngram, next_ngram )
 
-for elem in verbs:
-    print elem
-"""
+    #G.add_weighted_edges_from([(1,2,0.125),(1,3,0.75),(2,4,1.2),(3,4,0.375)])
+
+
+print "drawing..."
+nx.draw(G)
+print "save plot..."
+plt.savefig("path.png")
+
+#print G.nodes()
