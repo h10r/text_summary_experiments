385b73184d168fd58c30c831e260c50d45438b15
Hendrik Heuer <hendrikheuer@jaz5.kyla.fi>
2014-09-17 20:23:48 +0300

    added todo

--- main.py
+++ main.py
@@ -1,22 +1,19 @@
 # -*- coding: utf-8 -*-
 
 import string
-import nltk
-from nltk.stem import WordNetLemmatizer
 import numpy as np
 import networkx as nx
 import matplotlib.pyplot as plt
 
+with open('common_words.txt', 'r') as f:
+    stopwords = f.read().split()
+
 with open('pulman.txt', 'r') as f:
     article = f.read().split()
 
-STOP_WORDS = nltk.corpus.stopwords.words('english') + list( string.punctuation )
-
-#tokens = [i.decode('utf-8') for i in nltk.word_tokenize( article.lower() ) if i not in STOP_WORDS]
-
 G = nx.Graph()
 
-#article = article[0:300]
+article = article[0:300]
 
 previous_word = ""
 
@@ -35,12 +32,18 @@ for index in xrange(len(article)-1):
         current_word = next_word
         next_word = article[ index + 1 ]
 
-        print previous_word, current_word, next_word
+        #print previous_word, current_word, next_word
 
         G.add_edge( previous_word, current_word )
         G.add_edge( current_word, next_word )
         
         G.add_edge( previous_word, next_word )
+
+# @TODO: Remove stopwords from graph
+
+# @TODO: Add word2vec weights
+
+# @TODO: Select and print most important sentences
 
 def most_important(G):
     """ returns a copy of G with
@@ -48,28 +51,28 @@ def most_important(G):
     according to the pagerank """ 
 
     ranking = nx.betweenness_centrality(G).items()
+    ranking = sorted(ranking, key=lambda r: r[1], reverse=True)
+
     print ranking
+
+def pagerank(G):
+    """ returns a copy of G with
+    the most important nodes
+    according to the pagerank """ 
+
+    ranking = nx.betweenness_centrality(G).items()
+    #print ranking
+    
     r = [x[1] for x in ranking]
     m = sum(r)/len(r) # mean centrality
     t = m*3 # threshold, we keep only the nodes with 3 times the mean
+
     Gt = G.copy()
+
     for k, v in ranking:
         if v < t:
             Gt.remove_node(k)
+            print v
     return Gt
 
-Gt = most_important(G) # trimming
-
-from pylab import show
-
-pos = nx.spring_layout(G)
-
-nx.draw_networkx_nodes(G,pos,node_color='b',alpha=0.2,node_size=8)
-nx.draw_networkx_edges(G,pos,alpha=0.1)
-
-nx.draw_networkx_nodes(Gt,pos,node_color='r',alpha=0.4,node_size=254)
-
-nx.draw_networkx_labels(Gt,pos,font_size=12,font_color='b')
-
-show()
-
+most_important(G)
