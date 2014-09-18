947229424118f68d6ab85c7f8e7e8d9483586739
Hendrik Heuer <hendrikheuer@dhcp-85-184.eduroam.aalto.fi>
2014-09-18 12:13:34 +0300

    adding basic ranking

--- main.py
+++ main.py
@@ -3,47 +3,10 @@
 import string
 import numpy as np
 import networkx as nx
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
+from nltk.stem.wordnet import WordNetLemmatizer
 
-with open('common_words.txt', 'r') as f:
-    stopwords = f.read().split()
-
-with open('pulman.txt', 'r') as f:
-    article = f.read().split()
-
-G = nx.Graph()
-
-article = article[0:300]
-
-previous_word = ""
-
-for index in xrange(len(article)-1):
-    
-    # remove stop words and punctation
-    article[ index ] = article[ index ].translate(string.maketrans("",""), string.punctuation).lower()
-
-    if index == 0:
-        current_word = article[ index ]
-        next_word = article[ index + 1 ]
-
-        G.add_edge( current_word, next_word )
-    else:
-        previous_word = current_word
-        current_word = next_word
-        next_word = article[ index + 1 ]
-
-        #print previous_word, current_word, next_word
-
-        G.add_edge( previous_word, current_word )
-        G.add_edge( current_word, next_word )
-        
-        G.add_edge( previous_word, next_word )
-
-# @TODO: Remove stopwords from graph
-
-# @TODO: Add word2vec weights
-
-# @TODO: Select and print most important sentences
+### Functions
 
 def most_important(G):
     """ returns a copy of G with
@@ -53,7 +16,7 @@ def most_important(G):
     ranking = nx.betweenness_centrality(G).items()
     ranking = sorted(ranking, key=lambda r: r[1], reverse=True)
 
-    print ranking
+    return ranking
 
 def pagerank(G):
     """ returns a copy of G with
@@ -75,4 +38,76 @@ def pagerank(G):
             print v
     return Gt
 
-most_important(G)
+### Load files
+
+with open('common_words.txt', 'r') as f:
+    stopwords = f.read().split()
+
+with open('pulman.txt', 'r') as f:
+    article = f.read().split()
+
+G = nx.Graph()
+
+lmtzr = WordNetLemmatizer()
+
+article = article[0:500]
+
+previous_word = ""
+
+for index in xrange(len(article)-1):
+    # remove punctation
+    article[ index ] = article[ index ].translate(string.maketrans("",""), string.punctuation)
+    article[ index ] = article[ index ].lower()
+
+    """
+    article[ index ] = lmtzr.lemmatize( article[ index ] )
+
+    if article[index] != lmtzr.lemmatize( article[ index ] ):
+        print article[index], lmtzr.lemmatize( article[ index ] )
+    """
+
+    if index == 0:
+        current_word = article[ index ]
+        next_word = article[ index + 1 ]
+
+        G.add_edge( current_word, next_word )
+    else:
+        previous_word = current_word
+        current_word = next_word
+        next_word = article[ index + 1 ]
+
+        #print previous_word, current_word, next_word
+
+        G.add_edge( previous_word, current_word )
+        G.add_edge( current_word, next_word )
+        
+        G.add_edge( previous_word, next_word )
+
+ranked = most_important(G)
+
+ranked_top_100_without_stopwords = []
+
+count = 0
+index = 0
+max_index = len( ranked )
+
+while count <= 42:
+    word,ranking = ranked[ index ]
+
+    if not word in stopwords:
+        ranked_top_100_without_stopwords.append( (word, ranking) )
+        count += 1
+
+    index += 1
+
+    if index >= max_index:
+        break
+
+for r in ranked_top_100_without_stopwords:
+    print r
+
+# @TODO: Add word2vec weights
+
+# @TODO: Select and print most important sentences
+
+
